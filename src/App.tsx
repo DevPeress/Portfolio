d4f869020bbd9c6a886ce2d46fc1c470006ab33f
Fabrício Peres <128615061+xPeres@users.noreply.github.com>
2025-04-22 18:43:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,44 +77,50 @@ function App() {
             <div className='flex relative w-full h-auto top-120 md:top-200 lg:top-280 items-center justify-center'>
               <div className='absolute text-[#FFFFFF] light:text-[#2B2B2B] text-[6vw] lg:text-[3vw]'>O que eu sei</div>
               <div className='absolute top-4 md:top-6 lg:top-10 text-[3vw] lg:text-[1.2vw] text-transparent bg-clip-text bg-gradient-to-r from-[#4FC3F7] to-[#FFFFFF] light:to-[#484E53]'>Meus conhecimentos!</div>
-              <img className='absolute top-20 md:top-30 lg:top-40' src={white ? exp3 : exp1} alt="" />
-              <img className='absolute top-50 md:top-95 lg:top-120' src={white ? exp4 : exp2} alt="" />
+              <img className='absolute top-15 md:top-30 lg:top-40' src={white ? exp3 : exp1} alt="" />
+              <img className='absolute top-60 md:top-95 lg:top-120' src={white ? exp4 : exp2} alt="" />
             </div>
 
             <div className='flex relative w-full h-auto top-220 md:top-400 lg:top-500 items-center justify-center'>
-              <div className='absolute bottom-90 text-[#FFFFFF] light:text-[#2B2B2B] text-[6vw] lg:text-[3vw]'>Projetos</div>
-              <div className='absolute top-10 text-[3vw] lg:text-[1.2vw] text-transparent bg-clip-text bg-gradient-to-r from-[#4FC3F7] to-[#FFFFFF] light:to-[#484E53]'>Alguns dos meus Projetos!</div>
-              <div className='relative h-100 w-300 top-20 items-center justify-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+              <div className='absolute bottom-85 text-[#FFFFFF] light:text-[#2B2B2B] text-[6vw] lg:text-[3vw]'>Projetos</div>
+              <div className='absolute top-15 text-[3vw] lg:text-[1.2vw] text-transparent bg-clip-text bg-gradient-to-r from-[#4FC3F7] to-[#FFFFFF] light:to-[#484E53]'>Alguns dos meus Projetos!</div>
+              <div className='relative h-100 w-300 top-30 items-center justify-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-4'>
                 <div className='text-left'>
-                  <img className='' src={skillz} alt="" />
-                  <div className='relative text-[#C1C1C1] top-2 text-[5vw] md:text-[4vw] lg:text-[1vw] light:text-[#484E53]'>Skillz</div>
-                  <div className='relative text-[#A9A9A9] top-5 text-[3vw] md:text-[2vw] lg:text-[.7vw] w-[40vw] md:w-[30vw] lg:w-[11vw] light:text-[#484E53]'>Pequeno site com front-end e back-end para registro de serviços</div>
-                  <img className='relative bottom-5 left-76 lg:left-[16.8vw] hover:scale-110' onClick={() => openInNewTab("https://github.com/xPeres/Sistema-de-Divulga-o-de-Empresas")} src={acess} alt="" />
+                  <img className='flex relative bottom-5 w-full' src={skillz} alt="" />
+                  <div className='relative text-[#C1C1C1] bottom-2 text-[5vw] md:text-[4vw] lg:text-[1vw] light:text-[#484E53]'>Skillz</div>
+                  <div className='flex relative items-center justify-between w-full'>
+                    <div className='relative text-[#A9A9A9] text-[3vw] md:text-[2vw] lg:text-[.7vw] w-[40vw] md:w-[30vw] lg:w-[11vw] light:text-[#484E53]'>Pequeno site com front-end e back-end para registro de serviços</div>
+                    <img className='relative hover:scale-110' onClick={() => openInNewTab("https://github.com/xPeres/Sistema-de-Divulga-o-de-Empresas")} src={acess} alt="" />
+                  </div>
                 </div>
                 <div className='text-left'>
-                  <img className='' src={inv} alt="" />
-                  <div className='relative text-[#C1C1C1] top-2 text-[5vw] md:text-[4vw] lg:text-[1vw] light:text-[#484E53]'>Inventário</div>
-                  <div className='relative text-[#A9A9A9] top-5 text-[3vw] md:text-[2vw] lg:text-[.7vw] w-[40vw] md:w-[30vw] lg:w-[11vw] light:text-[#484E53]'>Sistema de Inventário para FIVEM que utiliza Drag UI.</div>
-                  <img className='relative bottom-5 left-76 lg:left-[16.8vw] hover:scale-110' onClick={() => openInNewTab("https://youtu.be/F0nvAkWIkDY")} src={acess} alt="" />
+                  <img className='flex relative bottom-5 w-full' src={inv} alt="" />
+                  <div className='relative text-[#C1C1C1] bottom-2 text-[5vw] md:text-[4vw] lg:text-[1vw] light:text-[#484E53]'>Inventário</div>
+                  <div className='flex relative items-center justify-between w-full'>
+                    <div className='relative text-[#A9A9A9] text-[3vw] md:text-[2vw] lg:text-[.7vw] w-[40vw] md:w-[30vw] lg:w-[11vw] light:text-[#484E53]'>Sistema de Inventário para FIVEM que utiliza Drag UI.</div>
+                    <img className='relative hover:scale-110' onClick={() => openInNewTab("https://youtu.be/F0nvAkWIkDY")} src={acess} alt="" />
+                  </div>
                 </div>
                 <div className='text-left'>
-                  <img className='' src={cal} alt="" />
-                  <div className='relative text-[#C1C1C1] top-2 text-[5vw] md:text-[4vw] lg:text-[1vw] light:text-[#484E53]'>Calculadora</div>
-                  <div className='relative text-[#A9A9A9] top-5 text-[3vw] md:text-[2vw] lg:text-[.7vw] w-[40vw] md:w-[30vw] lg:w-[11vw] light:text-[#484E53]'>Sistema de Calculadora simples com sistema de troca de cores.</div>
-                  <img className='relative bottom-5 left-76 lg:left-[16.8vw] hover:scale-110' onClick={() => openInNewTab("https://github.com/xPeres/Calculadora")} src={acess} alt="" />
+                  <img className='flex relative bottom-5 w-full' src={cal} alt="" />
+                  <div className='relative text-[#C1C1C1] bottom-2 text-[5vw] md:text-[4vw] lg:text-[1vw] light:text-[#484E53]'>Calculadora</div>
+                  <div className='flex relative items-center justify-between w-full'>
+                    <div className='relative text-[#A9A9A9] text-[3vw] md:text-[2vw] lg:text-[.7vw] w-[40vw] md:w-[30vw] lg:w-[11vw] light:text-[#484E53]'>Sistema de Calculadora simples com sistema de troca de cores.</div>
+                    <img className='relative hover:scale-110' onClick={() => openInNewTab("https://github.com/xPeres/Calculadora")} src={acess} alt="" />
+                  </div>
                 </div>
               </div>
             </div>
           </main>
 
-          <footer className='flex relative w-full h-35 md:h-25 lg:h-25 top-400 md:top-520 lg:top-550 border-t border-[#484E53]'>
-            <div className='flex relative items-center justify-between w-[80%] left-[10%]'>
-              <div className='text-[3vw] md:text-[1.5vw] lg:text-[1.2vw] text-[#A9A9A9] light:text-[#282938] top-10'>© 2025 todos os direitos reservados</div>
-              <div className='flex top-9 md:top-8 lg:top-[1.5vw] w-40 md:w-60 lg:w-[20vw] h-7 md:h-10 lg:h-[2.5vw] border-1 rounded border-[#484E53] items-center justify-center'>
-                <img className='w-[7vw] md:w-[2vw] lg:w-[2vw]' src={email} alt="" />
-                <div className='text-[#C9C9C9] light:text-[#484E53] text-[2vw] md:text-[1.5vw] lg:text-[1vw]' onClick={copiar}>fabricioperesdsantos@gmail.com</div>
+          <footer className='flex relative w-full h-35 md:h-25 lg:h-25 top-[420vw] md:top-520 lg:top-550 border-t border-[#484E53]'>
+            <div className='flex relative items-center justify-between w-[90%] lg:w-[80%] left-[5%] lg:left-[10%]'>
+              <div className='text-[1.5vw] md:text-[1.5vw] lg:text-[1.2vw] text-[#A9A9A9] light:text-[#282938] top-10'>© 2025 todos os direitos reservados</div>
+              <div className='flex top-9 md:top-8 lg:top-[1.5vw] w-35 md:w-60 lg:w-[20vw] h-7 md:h-10 lg:h-[2.5vw] border-1 rounded border-[#484E53] items-center justify-center'>
+                <img className='w-[4vw] md:w-[2vw] lg:w-[2vw]' src={email} alt="" />
+                <div className='text-[#C9C9C9] light:text-[#484E53] text-[1.5vw] md:text-[1.5vw] lg:text-[1vw]' onClick={copiar}>fabricioperesdsantos@gmail.com</div>
               </div>
-              <div className='flex top-20 md:top-8 lg:top-[2.1vw] items-center justify-between md:w-50 lg:w-80'>
+              <div className='flex top-20 md:top-8 lg:top-[2.1vw] items-center justify-between w-40 md:w-40 lg:w-80'>
                 <img src={white ? linkedin2 : linkedin} onClick={() => openInNewTab("https://www.linkedin.com/in/devperes")} alt="" />
                 <img src={white ? site2 : site} onClick={() => openInNewTab("https://discord.gg/maze-store-2k-937899788735746129")} alt="" />
                 <img src={white ? insta2 : insta} onClick={() => openInNewTab("https://www.instagram.com/fah.peres/")} alt="" />
